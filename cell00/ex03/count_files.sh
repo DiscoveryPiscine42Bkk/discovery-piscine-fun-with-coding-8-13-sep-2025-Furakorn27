d62806224d313e4a9fc21c0d2c462f13c7dc5dcc
Furakorn27 <furakorn48@gmail.com>
2025-09-11 19:56:23 +0700

    ex

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-find . | wc -l
+
+# นับไฟล์ปกติ
+file_count=$(find . -maxdepth 1 -type f | wc -l)
+
+# นับไดเรกทอรี
+dir_count=$(find . -maxdepth 1 -type d | wc -l)
+
+# รวมทั้งหมด (ไฟล์ + โฟลเดอร์)
+total=$((file_count + dir_count))
+
+# แสดงผล
+echo $total
